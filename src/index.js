ecb514d28b7524b5220a6de464db82e4a1009e3f
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-17 11:06:41 -0700

    File adjustments and index.js render

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//Renders react to the internet browser window
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);

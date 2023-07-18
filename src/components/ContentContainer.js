62eed5edb9b7cfc37ae29ec1001db3057695b6a0
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-18 09:57:59 -0700

    Conditionals

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -7,22 +7,30 @@ import Resume from './pages/Portfolio';
 import Header from './Header';
 
 export default function ContentContainer() {
-    const [currentContent, setCurrentContent] = useState('About');
+  const [currentContent, setCurrentContent] = useState('About');
 
-    const renderContent = () => {
-        if (currentContent === 'About') {
-            return <About />;
-        }
+  const renderContent = () => {
+    if (currentContent === 'Resume') {
+      return <Resume />;
     }
+    if (currentContent === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentContent === 'Contact') {
+      return <Contact />;
+    }
+    return <About />
+  }
 
-    const handleContentChange = (content) => setCurrentContent(content);
 
-    return (
-        <div>
-          <Header>
-            <Nav currentContent={currentContent} handleContentChange={handleContentChange} />
-          </Header>
-          {renderContent}
-        </div>
-      );
+  const handleContentChange = (content) => setCurrentContent(content);
+
+  return (
+    <div>
+      <Header>
+        <Nav currentContent={currentContent} handleContentChange={handleContentChange} />
+      </Header>
+      {renderContent}
+    </div>
+  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Nav from './Nav';
 import '../App.css'
 import image from '../Images/texturedbackground.jpg'
 

17ac51252633360a25c664a402b36407640d0521
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-20 17:13:55 -0700

    REACT About and CSS modified

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function About() {
     return (
-        <div>
+        <div className='container'>
             <h2 className='contentHeader'>About Me</h2>
             <p>
                 Voluptate magna aliqua laboris excepteur ipsum dolor mollit in. Labore ex veniam velit dolore ipsum eu proident irure laboris laboris consectetur sunt. Consectetur deserunt veniam in qui nulla adipisicing incididunt et occaecat officia officia. Culpa esse id Lorem nulla in id fugiat ex. Reprehenderit quis qui consequat irure eu et eu sint cupidatat culpa adipisicing in Lorem. Esse nulla sint excepteur ullamco ex nulla.Sint do laborum fugiat aute minim reprehenderit et est elit. Laborum minim ipsum amet proident irure deserunt minim voluptate fugiat cupidatat. Incididunt velit commodo mollit ut officia laboris occaecat sunt. Pariatur ipsum aliquip aliquip in eu. Ullamco magna laborum incididunt quis ut dolor. Esse do id deserunt labore pariatur amet sint ullamco reprehenderit qui est proident.
